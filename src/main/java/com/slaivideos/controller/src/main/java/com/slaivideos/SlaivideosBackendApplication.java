0b3f981ad13ca8c2ff7d8c56626c39eda8dc951e
SmileLabsAI <SMILELABSAI@GMAIL.COM>
2025-01-23 14:40:51 -0300

    Create SlaivideosBackendApplication.java

---
+++ src/main/java/com/slaivideos/controller/src/main/java/com/slaivideos/SlaivideosBackendApplication.java
@@ -0,0 +1,11 @@
+package com.slaivideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaivideosBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SlaivideosBackendApplication.class, args);
+    }
+}
