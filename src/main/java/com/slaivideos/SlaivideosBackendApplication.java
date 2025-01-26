5ae35827bdce9f08d71faa24d83fd36c0ad29bff
Smile Labs AI <smilelabsai@gmail.com>
2025-01-26 16:26:06 -0300

    Corrigindo erro no ComponentScan

--- src/main/java/com/slaivideos/SlaivideosBackendApplication.java
+++ src/main/java/com/slaivideos/SlaivideosBackendApplication.java
@@ -1,10 +1,10 @@
 package com.slaivideos;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan("com.slaivideos")
 public class SlaivideosBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(SlaivideosBackendApplication.class, args);
