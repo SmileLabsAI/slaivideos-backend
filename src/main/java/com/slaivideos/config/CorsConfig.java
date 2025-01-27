0fd7bbbd43817e6a193691ba21564a3acd493be5
Smile Labs AI <smilelabsai@gmail.com>
2025-01-27 20:24:21 -0300

    Commit

--- src/main/java/com/slaivideos/config/CorsConfig.java
+++ src/main/java/com/slaivideos/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.slaivideos.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,9 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://smilelabsai.github.io") // Permitir o domínio do frontend
+                .allowedOriginPatterns("https://smilelabsai.github.io") // allow o frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // Permite cookies e autenticação
+                .allowCredentials(true);
     }
 }
