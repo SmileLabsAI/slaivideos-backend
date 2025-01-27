e88515177940c2e2495547bf331198eede13083d
Smile Labs AI <smilelabsai@gmail.com>
2025-01-27 20:33:14 -0300

    Commit

--- src/main/java/com/slaivideos/config/CorsConfig.java
+++ src/main/java/com/slaivideos/config/CorsConfig.java
@@ -8,11 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://smilelabsai.github.io") // allow o frontend
+                .allowedOrigins("https://smilelabsai.github.io")  // Permitir apenas o frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
